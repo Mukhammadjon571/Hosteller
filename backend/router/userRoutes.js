19b156680e4d5c10d578435e9e5b5721050fd117
Mukhammadjon Kayumov <kayumovmukhammadjon@gmail.com>
2022-12-06 22:27:50 +0500

    Adding Backend

---
+++ backend/router/userRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const userController = require('../controller/userContoller');
+const authController = require('../controller/authContoller');
+
+const router = express.Router();
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
+
+// Protect all routes after this middleware
+router.use(authController.protect);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
+
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+module.exports = router;
